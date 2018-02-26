8c5e2102609924e029a449e204161f651df095df
784456700@qq.com <784456700@qq.com>
2018-02-26 14:59:58 +0800

    增量+全量

--- Weather/HisoryWeatherData.py
+++ Weather/HisoryWeatherData.py
@@ -72,6 +72,7 @@ if __name__ == '__main__':
                     getInfo()
                     time.sleep(3)
                 except :
+                    pass
                     print("IP被封"+ idNumber)
         idNumber=inforead.readline()
         idName=nameforead.readline()
